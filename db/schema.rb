6f90486904af5fffa6d712f5b6c5df3c2de7b437
daisuke_nabatame <nabanaba1225@gmail.com>
2020-10-16 11:49:05 +0900

    add_reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_093423) do
+ActiveRecord::Schema.define(version: 2020_10_16_022956) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
